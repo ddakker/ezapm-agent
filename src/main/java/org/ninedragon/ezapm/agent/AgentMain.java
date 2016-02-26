69481457602b1a31bce44215ed73aae7af4d02d2
ddakker <ddakker@naver.com>
2016-02-26 13:53:40 +0900

    백업 차원

--- src/main/java/org/ninedragon/ezapm/agent/AgentMain.java
+++ src/main/java/org/ninedragon/ezapm/agent/AgentMain.java
@@ -1,37 +1,91 @@
 package org.ninedragon.ezapm.agent;
 
+import org.ninedragon.ezapm.agent.send.netty.NettyClient;
+import org.ninedragon.ezapm.agent.timer.MBeanTimer;
 import org.ninedragon.ezapm.agent.transformer.ServletTransformer;
 
+import java.io.*;
 import java.lang.instrument.Instrumentation;
+import java.net.InetAddress;
+import java.util.Properties;
+import java.util.Timer;
 
 /**
  * Created by ddakker on 2016-02-19.
  */
 public class AgentMain {
-    private static AgentMain agentMain;
+    private static Instrumentation instrumentation;
 
-    private Instrumentation instrumentation;
+    Timer timer = new Timer("EZAPM TIMER", true);
 
-    public static boolean isExec 	= false;
     public static boolean isDebug 	= false;
+    public static boolean isExec 	= false;
 
-    public static void premain(String args, Instrumentation instrumentation) throws Exception {
-        if (isExec == false) {
-            boolean isRequest = args != null && args.contains("isRequest=true");
-            boolean isMBean = args != null && args.contains("isMBean=true");
-            boolean isLog = args != null && args.contains("isLog=true");
-            isDebug = args != null && args.contains("isDebug=true");
+    public static void premain(String args, Instrumentation inst) throws Exception {
+        if (AgentMain.isExec == true) {
+            return;
+        }
+        if (AgentMain.instrumentation != null) {
+            return;
+        }
+        /*String pathConf = System.getProperty("ezapm.conf");
+        if (pathConf == null) {
+            System.setProperty("ezapm.conf", args);
+        }
+        new Thread(Logger.initializer).start();
+        Logger.println("args: " + args);
+        intro(args);*/
 
-            //instrumentation.addTransformer(new TomcatTransformer());
-            agentMain = new AgentMain(instrumentation);
-            agentMain.start(isRequest, isMBean, isLog);
+
+        /*boolean isRequest = args != null && args.contains("isRequest=true");
+        boolean isMBean = args != null && args.contains("isMBean=true");
+        boolean isLog = args != null && args.contains("isLog=true");
+        isDebug = args != null && args.contains("isDebug=true");*/
+
+        /*String serverNm     = Conf.getProperty("server.nm");
+        boolean isRequest   = Boolean.parseBoolean(Conf.getProperty("is.request"));
+        boolean isMBean     = Boolean.parseBoolean(Conf.getProperty("is.mbean"));
+        boolean isLog       = Boolean.parseBoolean(Conf.getProperty("is.log"));
+        isDebug             = Boolean.parseBoolean(Conf.getProperty("is.debug"));*/
+        //String serverNm     = Conf.getProperty("server.nm");
+        boolean isRequest   = true;
+        boolean isMBean     = false;
+        boolean isLog       = false;
+        isDebug             = false;
+
+        NettyClient.init();
+
+        AgentMain.instrumentation = inst;
+
+        //instrumentation.addTransformer(new TomcatTransformer());
+        AgentMain agentMain = new AgentMain(instrumentation);
+        agentMain.start(isRequest, isMBean, isLog);
+
+        AgentMain.isExec				= true;
+    }
+
+    private static void intro(String conf) {
+        try {
+            System.setProperty("scouter.enabled", "true");
+            String nativeName = AgentMain.class.getName().replace('.', '/') + ".class";
+            ClassLoader cl = AgentMain.class.getClassLoader();
+            if (cl == null) {
+                System.out.println("loaded by system classloader ");
+                System.out.println("1: " + ClassLoader.getSystemClassLoader());
+                System.out.println("2: " + ClassLoader.getSystemClassLoader().getResource(nativeName));
+            } else {
+                System.out.println("loaded by app classloader ");
+                System.out.println("3: " + cl.getResource(nativeName));
+            }
+        } catch (Throwable t) {
         }
 
-        isExec = true;
+
+
     }
 
     public AgentMain(Instrumentation instrumentation) {
-        this.instrumentation = instrumentation;
+        AgentMain.instrumentation = instrumentation;
     }
 
     public final void start(boolean isRequest, boolean isMBean, boolean isLog) {
@@ -43,8 +97,9 @@ public class AgentMain {
         System.out.println("| author\t: ddakker@naver.com");
         System.out.println("==============================================");
 
+
         if (isRequest) {
-            this.instrumentation.addTransformer(new ServletTransformer());
+            AgentMain.instrumentation.addTransformer(new ServletTransformer());
         }
         if (isMBean) {
             //setupTimer();
@@ -52,5 +107,12 @@ public class AgentMain {
         if (isLog) {
             //setupLogWatch();
         }
+        System.setProperty("is.start.ezapm", "true");
+    }
+
+    private void setupTimer() {
+        System.out.println("startTimer");
+        MBeanTimer mBeanTimer = new MBeanTimer();
+        timer.scheduleAtFixedRate(mBeanTimer, 5000, 5000);
     }
 }
---
+++ src/main/java/org/ninedragon/ezapm/agent/Logger.java
@@ -0,0 +1,87 @@
+package org.ninedragon.ezapm.agent;
+
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by ddakker on 2016-02-26.
+ */
+public class Logger {
+    static PrintWriter pw = null;
+    static File logfile = null;
+
+    public static void println(String msg) {
+        println(msg, null);
+    }
+    public static void println(String msg, Throwable t) {
+        try {
+            if (pw != null) {
+                pw.println(msg);
+                if (t != null) {
+                    pw.println(getStackTrace(t));
+                }
+                pw.flush();
+                return;
+            }
+        } catch (Throwable e) {
+            System.err.println(e.getMessage());
+        }
+    }
+
+
+    static Runnable initializer = new Runnable() {
+
+        public void run() {
+            System.out.println("111");
+            try {
+                process();
+                Logger.println("2222");
+            } catch (Throwable t) {
+            }
+        }
+
+        private synchronized void process() {
+            try {
+                File root = new File("./logs");
+                if (root.canWrite() == false) {
+                    root.mkdirs();
+                }
+                if (root.canWrite() == false) {
+                    return;
+                }
+
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyyMMdd");
+                File file = new File(root, "ezapm-" + formatter.format(new Date()) + ".log");
+                System.out.println("file: " + file);
+                FileWriter fw = new FileWriter(file, true);
+                pw = new PrintWriter(fw);
+                logfile = file;
+            } catch (Throwable t) {
+                System.err.println(t.getMessage());
+            }
+        }
+    };
+
+    private static String getStackTrace(Throwable t) {
+        String CRLF = System.getProperty("line.separator");
+        StringBuffer sb = new StringBuffer();
+        sb.append(t.toString() + CRLF);
+        StackTraceElement[] se = t.getStackTrace();
+        if (se != null) {
+            for (int i = 0; i < se.length; i++) {
+                if (se[i] != null) {
+                    sb.append("\t" + se[i].toString());
+                    if (i != se.length - 1) {
+                        sb.append(CRLF);
+                    }
+                }
+            }
+        }
+
+        return sb.toString();
+    }
+}
